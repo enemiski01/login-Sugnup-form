ec3176b0bb00ab0792d65a4cdf7771a3a34c6fa6
Enemiski01 <adamaenema@gmail.com>
2020-10-23 07:24:39 -0700

    initial commit

--- js/custom.js
+++ js/custom.js
@@ -1,70 +1,70 @@
-$(document).ready(function() {
- 
-//Form Validation
+$(document).ready(function () {
 
-$('#email').on('focusout', function() {
+    //Form Validation
 
-    function validateEmail($email) {
-        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        return emailReg.test($email);
-    }
+    $('#email').on('focusout', function () {
 
-    if($(this).val() != "") {
-
-        if(validateEmail($(this).val())) {
-            $(this).css('border', '1px solid green')
-            $('#error').text('');
-        } else {
-            $(this).css('border', '1px solid red')
-            $('#error').text('Invalid email Address');
+        function validateEmail($email) {
+            var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            return emailReg.test($email);
         }
 
-    } else {
-        $(this).css('border', '1px solid red')
-        $('#error').text('Email is required');
-    }
+        if ($(this).val() != "") {
 
-});
+            if (validateEmail($(this).val())) {
+                $(this).css('border', '1px solid green')
+                $('#error').text('');
+            } else {
+                $(this).css('border', '1px solid red')
+                $('#error').text('Invalid email Address');
+            }
 
-$('#password').on('focusout', function() {
+        } else {
+            $(this).css('border', '1px solid red')
+            $('#error').text('Email is required');
+        }
 
-    if($(this).val() != "") {
-        
-        $(this).removeClass('is-invalid').addClass('is-valid')
-        $('#error').text('');
+    });
 
-    }else {
-        $(this).addClass('is-invalid')
-        $('#error').text("Password is required");
-    }
+    $('#password').on('focusout', function () {
 
-});
+        if ($(this).val() != "") {
 
-$('#password2').on('focusout', function() {
-
-    if($(this).val() != "") {
-        
-        $(this).removeClass('is-invalid').addClass('is-valid')
-        $('#error').text('');
-
-    }else {
-        $(this).addClass('is-invalid')
-        $('#error').text("Re-Password is required");
-    }
-
-});
-
-   $('#name').on('focusout', function() {
-
-    if($(this).val() != "") {
-        
-        $(this).css('border', '1px solid green')
+            $(this).removeClass('is-invalid').addClass('is-valid')
             $('#error').text('');
 
-    }else {
-        $(this).css('border', '1px solid rgb(212, 212, 212)')
-    }
+        } else {
+            $(this).addClass('is-invalid')
+            $('#error').text("Password is required");
+        }
 
-})
+    });
+
+    $('#password2').on('focusout', function () {
+
+        if ($(this).val() != "") {
+
+            $(this).removeClass('is-invalid').addClass('is-valid')
+            $('#error').text('');
+
+        } else {
+            $(this).addClass('is-invalid')
+            $('#error').text("Re-Password is required");
+        }
+
+    });
+
+    $('#name').on('focusout', function () {
+
+        if ($(this).val() != "") {
+
+            $(this).css('border', '1px solid green')
+            $('#error').text('');
+
+        } else {
+            $(this).css('border', '1px solid rgb(212, 212, 212)')
+        }
+
+    })
 
 });
